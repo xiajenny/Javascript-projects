d15cbe45accd57e4d7f1f58d979bbe707020c585
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-03-04 19:57:52 -0500

    first lecture

---
+++ script.js
@@ -0,0 +1,38 @@
+//Lecture: variables
+/*
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+
+//Lecture: variables 2
+var name = 'John';
+var age = 26;
+
+console.log(name + age);
+console.log(age+age);
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' year old ' +job + '. is he married? ' + isMarried);
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' year old ' +job + '. is he married? ' + isMarried);
+
+var lastName = prompt('WHat is the last name?');
