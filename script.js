c63a0ef319b2206664d4a20080acdba78800655b
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-03-10 17:31:28 -0500

    completed up to lecture 12

--- script.js
+++ script.js
@@ -15,24 +15,59 @@ console.log(fullAge);
 
 
 //Lecture: variables 2
+//var name = 'John';
+//var age = 26;
+//
+//console.log(name + age);
+//console.log(age + age);
+//
+//var job, isMarried;
+//
+//console.log(job);
+//
+//job = 'teacher';
+//isMarried = false;
+//
+//console.log(name + ' is a ' + age + ' year old ' +job + '. is he married? ' + isMarried);
+//
+//age = 'thirty six';
+//job = 'driver';
+//
+//console.log(name + ' is a ' + age + ' year old ' +job + '. is he married? ' + isMarried);
+//
+//var lastName = prompt('What is the last name?');
+//console.log(lastName);
+//
+//alert(name + ' is a ' + age + ' year old ' +job + '. is he married? ' + isMarried);
+
+
+
+
+//Lecture: operators
+//var now = 2018;
+//var birthYear = now - 26;
+//
+//birthYear = now - 26 * 2;
+//
+//console.log(birthYear);
+//
+//var ageJohn = 30;
+//var ageMark = 30;
+//
+//ageJohn = 3 + 5 * 4 -6;
+//
+//console.log(ageJohn);
+
+
+//Lecture: if/else statements
+
 var name = 'John';
 var age = 26;
+var isMarried = 'no';
 
-console.log(name + age);
-console.log(age+age);
-
-var job, isMarried;
-
-console.log(job);
-
-job = 'teacher';
-isMarried = false;
-
-console.log(name + ' is a ' + age + ' year old ' +job + '. is he married? ' + isMarried);
-
-age = 'thirty six';
-job = 'driver';
-
-console.log(name + ' is a ' + age + ' year old ' +job + '. is he married? ' + isMarried);
-
-var lastName = prompt('WHat is the last name?');
+if (isMarried === 'yes') {
+    console.log(name + ' is married!');
+}
+else{
+    console.log(name + ' will hopefully marry soon');
+}
